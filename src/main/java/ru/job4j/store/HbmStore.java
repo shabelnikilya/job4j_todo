4baaa19cd83e21838d81b95701d62351590882e1
shabelnikilya <shabelnik_ilya@mail.ru>
2022-02-10 14:10:45 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/store/HbmStore.java
+++ src/main/java/ru/job4j/store/HbmStore.java
@@ -2,15 +2,20 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.job4j.model.Item;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
+    private static final Logger LOG = LoggerFactory.getLogger(HbmStore.class);
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure().build();
     private final SessionFactory sf;
@@ -29,68 +34,76 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public Collection<Item> findAllItems() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery(
-                "from ru.job4j.model.Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.createQuery("from ru.job4j.model.Item").list());
     }
 
     @Override
     public Collection<Item> findFilterItems(boolean isDone) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery(
-                "from ru.job4j.model.Item where done = " + isDone).list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session ->
+                session.createQuery(
+                        "from ru.job4j.model.Item where done = " + isDone).list()
+                );
     }
 
     @Override
     public void addItem(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        this.makeTransaction(session -> session.save(item));
     }
 
     @Override
     public void removeItem(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = new Item();
-        item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        this.makeTransaction(session -> {
+                                          Item item = new Item();
+                                          item.setId(id);
+                                          session.delete(item);
+                                        });
     }
 
     @Override
     public void updateItem(int id, Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        item.setId(id);
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        this.makeTransaction(session -> {
+                                        item.setId(id);
+                                        session.update(item);
+                                        });
     }
 
     @Override
     public Item findItemById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return this.tx(session -> session.get(Item.class, id));
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    private <T> T tx(Function<Session, T> command) {
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        T rsl = null;
+        try {
+            rsl = command.apply(session);
+            tx.commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            LOG.error("Error with transaction", e);
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    private void makeTransaction(Consumer<Session> command) {
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            command.accept(session);
+            tx.commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            LOG.error("Error with transaction", e);
+        } finally {
+            session.close();
+        }
     }
 }
