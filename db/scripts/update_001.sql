902e1c115a02eb542149593d2e97b8603bcead48
shabelnikilya <shabelnik_ilya@mail.ru>
2022-02-11 18:46:39 +0300

    0. ToOne [#6873]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS items (
-	id SERIAL PRIMARY KEY,
-	description TEXT,
-	created TIMESTAMP,
-	done BOOLEAN,
-	users_id INT NOT NULL REGERENCES users(id)
-);
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     first_name TEXT,
     second_name TEXT,
     email TEXT,
     password TEXT,
     registration TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS items(
+	id SERIAL PRIMARY KEY,
+	description TEXT,
+	created TIMESTAMP,
+	done BOOLEAN,
+	users_id INT NOT NULL REFERENCES users(id)
 );
